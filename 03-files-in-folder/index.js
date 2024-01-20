95da29503ca448ca1d412b2630b279c17a63d201
Izy <126877709+izy-code@users.noreply.github.com>
2024-01-20 16:24:28 +0300

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const { resolve, parse } = require('path');
+const { readdir, stat } = require('fs/promises');
+
+const FOLDER_NAME = 'secret-folder';
+
+const displayFilesData = async () => {
+  try {
+    const folderPath = resolve(__dirname, FOLDER_NAME);
+    const names = await readdir(folderPath);
+
+    for (const name of names) {
+      const path = resolve(folderPath, name);
+      const stats = await stat(path);
+
+      if (!stats.isFile()) continue;
+
+      const parsedPath = parse(path);
+      const fileExt = parsedPath.ext.slice(1);
+
+      console.log(`${parsedPath.name} - ${fileExt} - ${stats.size} bytes`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+displayFilesData();
